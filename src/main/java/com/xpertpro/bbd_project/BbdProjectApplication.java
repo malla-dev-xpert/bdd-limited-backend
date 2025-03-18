000b9247f5f38de9152fb98ae49a9812ac4a0757
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-03-18 08:56:12 +0000

    first commit

---
+++ src/main/java/com/xpertpro/bbd_project/BbdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xpertpro.bbd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpertpro/bbd_project/BbdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpertpro.bbd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
