d8e28e0afcd2b3758f12e7b3bafc3ce04d49d7ef
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-03-21 15:12:57 +0000

    update email template

---
+++ src/main/java/com/xpertpro/bbd_project/config/ThymeleafConfig.java
@@ -0,0 +1,28 @@
+package com.xpertpro.bbd_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig {
+    @Bean
+    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.setEnableSpringELCompiler(true); // Active l'évaluation des expressions Spring EL
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/"); // Chemin des templates Thymeleaf
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+}
--- src/main/java/com/xpertpro/bbd_project/services/UserService.java
+++ src/main/java/com/xpertpro/bbd_project/services/UserService.java
@@ -7,11 +7,15 @@ import com.xpertpro.bbd_project.enums.StatusEnum;
 import com.xpertpro.bbd_project.mapper.UserDtoMapper;
 import com.xpertpro.bbd_project.repository.RoleRepository;
 import com.xpertpro.bbd_project.repository.UserRepository;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.util.Optional;
 
@@ -20,6 +24,8 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
+    @Autowired
+    SpringTemplateEngine templateEngine;
     @Autowired
     private UserDtoMapper userMapper;
     @Autowired
@@ -78,36 +84,59 @@ public class UserService {
         user.setRole(role);
         user.setPassword(encodePassword(randomPassword));
         userRepository.save(user);
+        try {
+            if (user.getEmail() != null && user.getStatusEnum() == StatusEnum.CREATE) {
+                Context context = new Context();
+                context.setVariable("firstName", user.getFirstName());
+                context.setVariable("lastName", user.getLastName());
+                context.setVariable("username", user.getUsername());
+                context.setVariable("password", clearTextPassword);
+                context.setVariable("roleName", user.getRole().getName());
+                context.setVariable("createdAt", user.getCreatedAt());
 
-        try{
-            if(user.getEmail() != null){
-                if(user.getStatusEnum() == StatusEnum.CREATE){
-                    SimpleMailMessage message = new SimpleMailMessage();
-                    message.setTo(user.getEmail());
-                    message.setSubject("Bienvenue sur BBD-LIMITED – Vos informations de connexion");
-                    message.setText
-                            ("Bonjour "+ user.getFirstName() + " " + user.getLastName() + "\n" +
-                                    "Nous sommes ravis de vous accueillir sur BBD-LIMITED ! Votre compte a été créé avec succès. Vous trouverez ci-dessous vos informations de connexion :"+
-                                    "\n"+
-                                    "\n"+
-                                    "Username : "+user.getUsername()+"\n"+
-                                    "Mot de passe : " +clearTextPassword+"\n"+
-                                    "Fait le : "+ user.getCreatedAt()+
-                                    "\n"+
-                                    "\n"+
-                                    "\n"+
-                                    "Nous vous recommandons de modifier votre mot de passe dès votre première connexion pour des raisons de sécurité."
-                                    + "\n" + "\n"+
-                                    "Si vous avez des questions ou besoin d’assistance, n’hésitez pas à nous contacter à bbdlimited@gmail.com."
-                                    + "\n"+"\n"+"\n"+
-                                    "Bienvenue et bonne expérience sur BBD LIMITED !"
-                            );
-                    mailSender.send(message);
-                }
+                String htmlContent = templateEngine.process("register", context);
+
+                MimeMessage mimeMessage = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                helper.setTo(user.getEmail());
+                helper.setSubject("Bienvenue sur BBD-LIMITED – Vos informations de connexion");
+                helper.setText(htmlContent, true);
+
+                mailSender.send(mimeMessage);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+//
+//        try{
+//            if(user.getEmail() != null){
+//                if(user.getStatusEnum() == StatusEnum.CREATE){
+//                    SimpleMailMessage message = new SimpleMailMessage();
+//                    message.setTo(user.getEmail());
+//                    message.setSubject("Bienvenue sur BBD-LIMITED – Vos informations de connexion");
+//                    message.setText
+//                            ("Bonjour "+ user.getFirstName() + " " + user.getLastName() + "\n" +
+//                                    "Nous sommes ravis de vous accueillir sur BBD-LIMITED ! Votre compte a été créé avec succès. Vous trouverez ci-dessous vos informations de connexion :"+
+//                                    "\n"+
+//                                    "\n"+
+//                                    "Username : "+user.getUsername()+"\n"+
+//                                    "Mot de passe : " +clearTextPassword+"\n"+
+//                                    "Fait le : "+ user.getCreatedAt()+
+//                                    "\n"+
+//                                    "\n"+
+//                                    "\n"+
+//                                    "Nous vous recommandons de modifier votre mot de passe dès votre première connexion pour des raisons de sécurité."
+//                                    + "\n" + "\n"+
+//                                    "Si vous avez des questions ou besoin d’assistance, n’hésitez pas à nous contacter à bbdlimited@gmail.com."
+//                                    + "\n"+"\n"+"\n"+
+//                                    "Bienvenue et bonne expérience sur BBD LIMITED !"
+//                            );
+//                    mailSender.send(message);
+//                }
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
 
         return "SUCCESS";
     }
