aa8374ff6b34f59ff39ab155b0914486a9c6284d
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-05-02 11:10:48 +0000

    delete item service

--- src/main/java/com/xpertpro/bbd_project/controllers/ItemController.java
+++ src/main/java/com/xpertpro/bbd_project/controllers/ItemController.java
@@ -1,8 +1,11 @@
 package com.xpertpro.bbd_project.controllers;
 
 import com.xpertpro.bbd_project.dto.ItemDto;
+import com.xpertpro.bbd_project.dto.harbor.HarborDto;
 import com.xpertpro.bbd_project.services.ItemServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,5 +21,20 @@ public class ItemController {
     @GetMapping("/package/{packageId}")
     public List<ItemDto> getItemsByPackageId(@PathVariable Long packageId) {
         return itemServices.getItemsByPackageId(packageId);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id, @RequestParam(name = "userId") Long userId, @RequestParam(name = "packageId") Long packageId){
+        String result = itemServices.deleteItem(id, userId, packageId);
+        switch (result) {
+            case "ITEM_NOT_FOUND":
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Article non trouve.");
+            case "PACKAGE_NOT_FOUND":
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Colis non trouve.");
+            case "USER_NOT_FOUND":
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Utilisateur non trouve.");
+            default:
+                return ResponseEntity.status(HttpStatus.CREATED).body("Article supprimer avec succès !");
+        }
     }
 }
--- src/main/java/com/xpertpro/bbd_project/services/ItemServices.java
+++ src/main/java/com/xpertpro/bbd_project/services/ItemServices.java
@@ -4,18 +4,28 @@ import com.xpertpro.bbd_project.dto.ItemDto;
 import com.xpertpro.bbd_project.dto.Package.PackageResponseDto;
 import com.xpertpro.bbd_project.entity.Items;
 import com.xpertpro.bbd_project.entity.Packages;
+import com.xpertpro.bbd_project.entity.UserEntity;
+import com.xpertpro.bbd_project.entity.Warehouse;
 import com.xpertpro.bbd_project.enums.StatusEnum;
 import com.xpertpro.bbd_project.repository.ItemsRepository;
+import com.xpertpro.bbd_project.repository.PackageRepository;
+import com.xpertpro.bbd_project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class ItemServices {
     @Autowired
     ItemsRepository itemsRepository;
+    @Autowired
+    PackageRepository packageRepository;
+    @Autowired
+    UserRepository userRepository;
 
     public List<ItemDto> getItemsByPackageId(Long packageId) {
         List<Items> items = itemsRepository.findByPackagesId(packageId);
@@ -30,5 +40,30 @@ public class ItemServices {
                     return dto;
                 })
                 .collect(Collectors.toList());
+    }
+
+    public String deleteItem(Long id, Long userId, Long packageId) {
+        Optional<Items> optionalItems = itemsRepository.findById(id);
+        Optional<Packages> optionalPackages = packageRepository.findById(packageId);
+        Optional<UserEntity> optionalUser = userRepository.findById(userId);
+        if (optionalItems.isEmpty()) {
+            return "ITEM_NOT_FOUND";
+        }
+
+        if (optionalPackages.isEmpty()) {
+            return "PACKAGE_NOT_FOUND";
+        }
+
+        if (optionalUser.isEmpty()) {
+            return "USER_NOT_FOUND";
+        }
+
+        Items items = optionalItems.get();
+
+        items.setStatus(StatusEnum.DELETE);
+        items.setUser(optionalUser.get());
+        itemsRepository.save(items);
+
+        return "DELETED";
     }
 }
