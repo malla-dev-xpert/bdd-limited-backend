145c599321118149d46dd594321c46bfe1cb8c36
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-05-23 11:18:02 +0000

    update

--- src/main/java/com/xpertpro/bbd_project/services/PartnerServices.java
+++ src/main/java/com/xpertpro/bbd_project/services/PartnerServices.java
@@ -1,5 +1,9 @@
 package com.xpertpro.bbd_project.services;
 
+import com.xpertpro.bbd_project.dto.ExpeditionDto;
+import com.xpertpro.bbd_project.dto.achats.AchatDto;
+import com.xpertpro.bbd_project.dto.achats.LigneAchatDto;
+import com.xpertpro.bbd_project.dto.achats.VersementDto;
 import com.xpertpro.bbd_project.dto.partners.PartnerDto;
 import com.xpertpro.bbd_project.dto.partners.UpdatePartnersDto;
 import com.xpertpro.bbd_project.entity.Partners;
@@ -51,7 +55,7 @@ public class PartnerServices {
         return "SUCCESS";
     }
 
-    public List<PartnerDto> getAllPartner(int page, String query) {
+    public List<PartnerDto> getAllCustomer(int page, String query) {
         int pageSize = 30;
         Pageable pageable = PageRequest.of(page, pageSize, Sort.by("createdAt").descending());
 
@@ -69,19 +73,105 @@ public class PartnerServices {
 
 
         return partners.stream()
-                .filter(pkg -> pkg.getStatus() != StatusEnum.DELETE)
+                .filter(partner -> partner.getStatus() != StatusEnum.DELETE)
+                .filter(partner -> partner.getAccountType().equals("CLIENT"))
                 .sorted(Comparator.comparing(Partners::getCreatedAt).reversed())
-                .map(pkg -> {
+                .map(partner -> {
                     PartnerDto dto = new PartnerDto();
-                    dto.setId(pkg.getId());
-                    dto.setAccountType(pkg.getAccountType());
-                    dto.setAdresse(pkg.getAdresse());
-                    dto.setCountry(pkg.getCountry());
-                    dto.setEmail(pkg.getEmail());
-                    dto.setFirstName(pkg.getFirstName());
-                    dto.setLastName(pkg.getLastName());
-                    dto.setPhoneNumber(pkg.getPhoneNumber());
-                    dto.setBalance(pkg.getBalance());
+                    dto.setId(partner.getId());
+                    dto.setAccountType(partner.getAccountType());
+                    dto.setAdresse(partner.getAdresse());
+                    dto.setCountry(partner.getCountry());
+                    dto.setEmail(partner.getEmail());
+                    dto.setFirstName(partner.getFirstName());
+                    dto.setLastName(partner.getLastName());
+                    dto.setPhoneNumber(partner.getPhoneNumber());
+                    dto.setBalance(partner.getBalance());
+
+                    List<VersementDto> versementDtoList = partner.getVersements().stream()
+                            .filter(item -> item.getStatus() != StatusEnum.DELETE)
+                            .map(v -> {
+                                VersementDto newVersemtDto = new VersementDto();
+                                newVersemtDto.setMontantVerser(v.getMontantVerser());
+                                newVersemtDto.setMontantRestant(v.getMontantRestant());
+                                newVersemtDto.setPartnerPhone(v.getPartner() != null ? v.getPartner().getPhoneNumber() : null);
+                                newVersemtDto.setPartnerCountry(v.getPartner() != null ? v.getPartner().getCountry() : null);
+                                newVersemtDto.setPartnerName(v.getPartner() != null ? v.getPartner().getFirstName()  + " " + v.getPartner().getLastName() : null);
+                                newVersemtDto.setPartnerAccountType(v.getPartner() != null ? v.getPartner().getAccountType() : null);
+                                newVersemtDto.setCreatedAt(v.getCreatedAt());
+                                newVersemtDto.setReference(v.getReference());
+
+                                List<AchatDto> achatDtos = v.getAchats().stream()
+                                        .filter(item -> item.getStatus() != StatusEnum.DELETE)
+                                        .map(item -> {
+                                            AchatDto achatDto = new AchatDto();
+                                            achatDto.setId(item.getId());
+                                            achatDto.setFournisseur(item.getFournisseur() != null
+                                                    ? item.getFournisseur().getFirstName() + " " + item.getFournisseur().getLastName()
+                                                    : null);
+                                            achatDto.setFournisseurPhone(item.getFournisseur() != null
+                                                    ? item.getFournisseur().getPhoneNumber()
+                                                    : null);
+                                            // Utilisation des montants du versement parent
+                                            achatDto.setMontantRestant(v.getMontantRestant());
+                                            achatDto.setMontantVerser(v.getMontantVerser());
+                                            achatDto.setReferenceVersement(v.getReference());
+
+                                            List<LigneAchatDto> ligneDtos = item.getLignes().stream()
+                                                    .map(ligne -> {
+                                                        LigneAchatDto ligneDto = new LigneAchatDto();
+                                                        ligneDto.setId(ligne.getId());
+                                                        ligneDto.setAchatId(ligne.getAchats() != null
+                                                                ? ligne.getAchats().getId()
+                                                                : null);
+                                                        ligneDto.setQuantity(ligne.getQuantite());
+                                                        ligneDto.setPrixTotal(ligne.getPrixTotal());
+                                                        ligneDto.setItemId(ligne.getItem() != null
+                                                                ? ligne.getItem().getId()
+                                                                : null);
+                                                        ligneDto.setDescriptionItem(ligne.getItem() != null
+                                                                ? ligne.getItem().getDescription()
+                                                                : null);
+                                                        ligneDto.setQuantityItem(ligne.getItem().getQuantity());
+                                                        ligneDto.setUnitPriceItem(ligne.getItem().getUnitPrice());
+                                                        return ligneDto;
+                                                    }).collect(Collectors.toList());
+
+                                            achatDto.setLignes(ligneDtos);
+
+                                            return achatDto;
+                                        }).collect(Collectors.toList());
+
+                                newVersemtDto.setAchats(achatDtos);
+
+                                return newVersemtDto;
+
+                            }).collect(Collectors.toList());
+
+                    List<ExpeditionDto> expeditionDtoList = partner.getExpeditions().stream()
+                            .filter(item -> item.getStatus() != StatusEnum.DELETE)
+                            .map(e -> {
+                                ExpeditionDto newExpeditionDto = new ExpeditionDto();
+                                newExpeditionDto.setItemQuantity(e.getItemQuantity());
+                                newExpeditionDto.setWeight(e.getWeight());
+                                newExpeditionDto.setClientName(e.getClient() != null ? e.getClient().getFirstName() + " " + e.getClient().getLastName() : null);
+                                newExpeditionDto.setClientPhone(e.getClient() != null ? e.getClient().getPhoneNumber() : null);
+                                newExpeditionDto.setExpeditionType(e.getExpeditionType());
+                                newExpeditionDto.setRef(e.getRef());
+                                newExpeditionDto.setCbn(e.getCbn());
+                                newExpeditionDto.setDestinationCountry(e.getDestinationCountry());
+                                newExpeditionDto.setStartCountry(e.getStartCountry());
+                                newExpeditionDto.setStartDate(e.getStartDate());
+                                newExpeditionDto.setArrivalDate(e.getArrivalDate());
+                                newExpeditionDto.setId(e.getId());
+                                newExpeditionDto.setStatus(e.getStatus().name());
+
+                                return newExpeditionDto;
+
+                            }).collect(Collectors.toList());
+
+                    dto.setVersements(versementDtoList);
+                    dto.setExpeditions(expeditionDtoList);
 
                     return dto;
                 })
@@ -89,9 +179,23 @@ public class PartnerServices {
 
     }
 
-    public Page<PartnerDto> findPartnersByType(int page, String type) {
+    public Page<PartnerDto> findPartnersByType(int page, String type, String query) {
+        int pageSize = 30;
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by("createdAt").descending());
+
         Page<Partners> partners = partnerRepository.findByAccountType(type, PageRequest.of(page, 50));
-        return partners.map(partner -> new PartnerDto(
+        if (query != null && !query.isEmpty()) {
+            partners = partnerRepository.findByStatusAndSearchQuery(
+                    StatusEnum.CREATE,
+                    "%" + query.toLowerCase() + "%",
+                    pageable
+            );
+        } else {
+            partners = partnerRepository.findByStatus(StatusEnum.CREATE, pageable);
+        }
+
+        return partners
+                .map(partner -> new PartnerDto(
                 partner.getId(),
                 partner.getFirstName(),
                 partner.getLastName(),
@@ -100,8 +204,18 @@ public class PartnerServices {
                 partner.getCountry(),
                 partner.getAdresse(),
                 partner.getAccountType(),
-                partner.getBalance())
-
+                partner.getBalance(),
+                partner.getVersements().stream().map(versements -> {
+                    VersementDto newDto = new VersementDto();
+                    newDto.setId(versements.getId());
+                    return newDto;
+                }).collect(Collectors.toList()),
+                partner.getExpeditions().stream().map(expeditions -> {
+                    ExpeditionDto newDto = new ExpeditionDto();
+                    newDto.setId(expeditions.getId());
+                    return newDto;
+                }).collect(Collectors.toList())
+                )
         );
     }
 
